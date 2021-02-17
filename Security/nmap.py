3f299001dff5ab151fa465c910bcab18d7a1137e
Marcio Gustavo Chahad <mchahad@gmail.com>
2021-02-16 22:26:38 -0300

    Apenas ajustes...

--- Security/nmap.py
+++ Security/nmap.py
@@ -12,15 +12,15 @@ options = {'top_ports': 'scan_top_ports', 'list_scan': 'nmap_list_scan', 'os_det
 print("NMAP options: A)" + options['top_ports'] + ", B)" + options['list_scan'] + ", C)" + options['os_detection'])
 choice = input("Please, choose one of NMAP options: ")
 
-if choice == 'A':
+if choice == 'A' or 'a':
     print("Running NMAP Scan Top Ports...")
     top_ports = nmap.scan_top_ports(target[:-1])
     pprint(top_ports)
-if choice == 'B':
+if choice == 'B' or 'b':
     print("Running NMAP List Scan...")
     list_scan = nmap.nmap_list_scan(target[:-1])
     pprint(list_scan)
-if choice == 'C':
+if choice == 'C' or 'c':
     print("Running NMAP OS Detection...")
     os_detection = nmap.nmap_os_detection(target[:-1])
     pprint(os_detection)
