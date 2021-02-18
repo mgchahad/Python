57d777e13f228a41e2d5bf7b353a38953044ed67
Marcio Gustavo Chahad <mchahad@gmail.com>
2021-02-18 09:05:32 -0300

    Ajustes no script

--- Security/nmap.py
+++ Security/nmap.py
@@ -8,21 +8,21 @@ nmap = nmap3.Nmap()
 target = subprocess.check_output("nmap --iflist | grep DST/MASK -A1 | grep -vE '(DST)' | awk '{print $1}'", shell=True, universal_newlines=True)
 print("Your network is: " + target[:-1])
 
-options = {'top_ports': 'scan_top_ports', 'list_scan': 'nmap_list_scan', 'os_detection': 'nmap_os_detection'}
-print("NMAP options: A)" + options['top_ports'] + ", B)" + options['list_scan'] + ", C)" + options['os_detection'])
+options = ['Scan Top Ports', 'List Scan', 'OS Detection']
+print("NMAP options: 1)" + options[0] + ", 2)" + options[1] + ", 3)" + options[2])
 choice = input("Please, choose one of NMAP options: ")
 
-if choice == 'A' or 'a':
+if  choice == '1':
     print("Running NMAP Scan Top Ports...")
     top_ports = nmap.scan_top_ports(target[:-1])
     pprint(top_ports)
 
-elif choice == 'B' or 'b':
+if  choice == '2':
     print("Running NMAP List Scan...")
     list_scan = nmap.nmap_list_scan(target[:-1])
     pprint(list_scan)
 
-elif choice == 'C' or 'c':
+if  choice == '3':
     print("Running NMAP OS Detection...")
     os_detection = nmap.nmap_os_detection(target[:-1])
     pprint(os_detection)
